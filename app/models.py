70479a05d7c565a30d861caf832ce610e307f7f5
lakhirMAlik <lakhirmalik43@gmail.com>
2026-06-06 20:00:44 +0500

    feat: add SQLAlchemy database connection and Student model

---
+++ app/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String
+from database import Base
+
+
+class Student(Base):
+    __tablename__ = "students"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    reg_no = Column(String, unique=True, nullable=False)
+    email = Column(String, nullable=True)
